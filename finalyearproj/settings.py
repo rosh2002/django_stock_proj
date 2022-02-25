2af26f496a6a43e0729fd0f4dac7cc2c45bf643e
Roshni <salianroshni2002@gmail.com>
2022-02-25 22:21:41 +0530

    Third commit

--- finalyearproj/settings.py
+++ finalyearproj/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -121,6 +122,6 @@ USE_TZ = True
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'pred_app/static')
 STATIC_URL = 'pred_app/static/'
-
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 DEFAULT_AUTO_FIELD= 'django.db.models.AutoField'
