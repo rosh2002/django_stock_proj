66d0665b54ad46c97e49b99cab49303df852f5d5
Roshni <salianroshni2002@gmail.com>
2022-02-25 20:16:57 +0530

    Second commit

--- finalyearproj/settings.py
+++ finalyearproj/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = 'e7s+*fg@po76%s@0hk!5l1$8v6)dpxxa=y5^e0vfl2($0ys#4u'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['127.0.0.1', 'predictonweb.herokuapp.com']
 
 
 # Application definition
