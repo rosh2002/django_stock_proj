1eeb1a809478746e25299763f836bdffe324bec4
Roshni <salianroshni2002@gmail.com>
2022-02-26 18:20:59 +0530

    Third commit

--- pred_app/lstm_prediction.py
+++ pred_app/lstm_prediction.py
@@ -8,7 +8,7 @@ def lstm_prediction(se, stock_symbol):
 		import yfinance as yf
 		yf.pdr_override()
 		if se == 'NSE': stock_symbol += ".NS" 
-		return pdr.get_data_yahoo(stock_symbol, period="5y")
+		return pdr.get_data_yahoo(stock_symbol, period="2y")
 
 
 	"""LSTM model development"""
@@ -49,7 +49,7 @@ def lstm_prediction(se, stock_symbol):
 	lstm_model.add(LSTM(units=50))
 	lstm_model.add(Dense(1))
 	lstm_model.compile(loss='mean_squared_error', optimizer='adam')
-	lstm_model.fit(x_totrain, y_totrain, epochs=1, batch_size=1, verbose=2)
+	lstm_model.fit(x_totrain, y_totrain, epochs=3, batch_size=1, verbose=2)
 	#predicting next data stock price
 	myinputs = new_seriesdata[len(new_seriesdata) - (100) - 60:].values
 	myinputs = myinputs.reshape(-1,1)
